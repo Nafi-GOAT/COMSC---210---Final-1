4402e2a6be43f5d97ccf014a911958c29935cdce
Nafi-GOAT <nafih2989@gmail.com>
2025-12-10 17:36:16 -0800

    Working on Milestone 1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//
+// Mehraj Hasan Nafi | COMSC 210 | Final 1
+//IDE used: VSCode 
+//
+//
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+    map<string, int > traffic;
+    ifstream infile
+}
